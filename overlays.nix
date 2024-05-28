46d53ca3b7f78cebe603f2fe659ac72a6daf25f5
Wuger <accounts+github@wuger.works>
2024-05-28 16:04:48 -0500

    refactor: fix derivation of overlay

--- overlays.nix
+++ overlays.nix
@@ -2,13 +2,12 @@
   imports = [
     inputs.flake-parts.flakeModules.easyOverlay
   ];
-  perSystem = {config, pkgs, ...}: let 
+  perSystem = {config, pkgs, ...}: let
+    mkOverlayAttr = name: value: pkgs.callPackage value; 
     generated = import ./sources/generated.nix;
   in {
-    overlayAttrs = {
-      xplr-plugins = generated {
+    overlayAttrs.xplr-plugins = builtins.mapAttrs mkOverlayAttr generated {
         inherit (pkgs) fetchurl fetchgit fetchFromGitHub dockerTools;
       };
-    };
   };
 }
